fa588ef2e64fecd4f4975e1e42f9b480765c4d7f
Akarsh Chilakala <103856524+AkarshChil@users.noreply.github.com>
2026-07-17 14:12:35 -0400

    Update BootStrap.sh

--- BootStrap.sh
+++ BootStrap.sh
@@ -1,26 +1,25 @@
 #!/bin/bash -l
 set -euo pipefail
 
-ENV_NAME="scaaml314"
-WORKDIR="${PSCRATCH}/PSCA"
-REPO="$WORKDIR/scaaml"
-
 module load conda
 source "$(conda info --base)/etc/profile.d/conda.sh"
 
-# Create the env if needed
-if ! conda env list | awk '{print $1}' | grep -qx "$ENV_NAME"; then
-    echo "Creating Conda environment..."
-    conda create -y -n "$ENV_NAME" python=3.14 pip
+ENV_DIR="${PSCRATCH}/conda-envs/scaaml312"
+WORKDIR="${PSCRATCH}/PSCA"
+REPO="$WORKDIR/scaaml"
+
+mkdir -p "$(dirname "$ENV_DIR")"
+mkdir -p "$WORKDIR"
+
+if [ ! -d "$ENV_DIR" ]; then
+    conda create -y -p "$ENV_DIR" python=3.12 pip
 fi
 
-conda activate "$ENV_NAME"
+conda activate "$ENV_DIR"
 
 echo "Python:"
 which python
 python --version
-
-mkdir -p "$WORKDIR"
 
 if [ -d "$REPO/.git" ]; then
     cd "$REPO"
@@ -31,15 +30,14 @@ else
 fi
 
 python -m pip install --upgrade pip setuptools wheel
-pip install -e .
-pip install chipwhisperer
-pip install sedpack
-pip install "tensorflow[and-cuda]"
+python -m pip install -e .
+python -m pip install chipwhisperer
+python -m pip install sedpack
+python -m pip install tensorflow
 
-echo
-echo "Environment ready."
-echo "To use it later:"
+echo "Done."
+echo "Activate later with:"
 echo "module load conda"
 echo "source \"\$(conda info --base)/etc/profile.d/conda.sh\""
-echo "conda activate $ENV_NAME"
+echo "conda activate $ENV_DIR"
 echo "cd $REPO"
