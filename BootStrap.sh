ebd9c9bc92dba096c827d52803f94fd1b530200e
Akarsh Chilakala <103856524+AkarshChil@users.noreply.github.com>
2026-07-17 14:03:13 -0400

    Update BootStrap.sh

--- BootStrap.sh
+++ BootStrap.sh
@@ -1,22 +1,24 @@
-#!/usr/bin/env bash
-#set -e
+#!/bin/bash -l
+set -euo pipefail
 
-ENV_NAME="scaaml311"
+ENV_NAME="scaaml314"
+WORKDIR="${PSCRATCH}/PSCA"
+REPO="$WORKDIR/scaaml"
 
-source "$HOME/anaconda3/etc/profile.d/conda.sh"
+module load conda
+source "$(conda info --base)/etc/profile.d/conda.sh"
 
+# Create the env if needed
 if ! conda env list | awk '{print $1}' | grep -qx "$ENV_NAME"; then
     echo "Creating Conda environment..."
-    conda create -y -n "$ENV_NAME" python=3.11 pip
+    conda create -y -n "$ENV_NAME" python=3.14 pip
 fi
 
 conda activate "$ENV_NAME"
 
 echo "Python:"
+which python
 python --version
-
-WORKDIR="/global/u1/a/achilaka/PSCA"
-REPO="$WORKDIR/scaaml"
 
 mkdir -p "$WORKDIR"
 
@@ -29,7 +31,6 @@ else
 fi
 
 python -m pip install --upgrade pip setuptools wheel
-
 pip install -e .
 pip install chipwhisperer
 pip install sedpack
@@ -37,7 +38,8 @@ pip install "tensorflow[and-cuda]"
 
 echo
 echo "Environment ready."
-echo
-echo "Run:"
-echo "conda activate scaaml311"
+echo "To use it later:"
+echo "module load conda"
+echo "source \"\$(conda info --base)/etc/profile.d/conda.sh\""
+echo "conda activate $ENV_NAME"
 echo "cd $REPO"
